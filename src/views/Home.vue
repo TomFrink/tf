d6e095784ca4beab9e096b1c8f331f6baf584d34
Tom Frink <tomfrink11@hotmail.com>
2019-04-08 22:55:16 -0400

    Add SVG

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,11 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <v-container>
+    <img src="../assets/TF-icon.svg" alt="TF"/>
+    <HelloWorld msg="Welcome to Tom's Vue.js App" />
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
-export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+import HelloWorld from "../components/HelloWorld"; export default { name:
+"home", components: { HelloWorld } };
 </script>
