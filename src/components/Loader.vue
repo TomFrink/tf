08c88e87feb9d331f4696ec82ac419218d83ebf3
TomFrink <tomfrink11@hotmail.com>
2019-09-20 09:47:54 -0400

    added codepen component

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -10,8 +10,7 @@
 <script>
 export default {
   name: "Loader",
-  props: [""],
-  mounted() {}
+  props: [""]
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
   <v-container>
     <HelloWorld msg="Welcome to Tom Frink's App" />
+    <codepen :options="codepenInput" />
   </v-container>
 </template>
 
 <script>
 import HelloWorld from "../components/HelloWorld";
-export default { name: "home", components: { HelloWorld } };
+import Codepen from "../components/Pen";
+
+export default {
+  name: "home",
+  components: {
+    HelloWorld,
+    Codepen
+  },
+  data() {
+    return {
+      codepenInput: {
+        "data-height": "100%",
+        "data-theme-id": "dark",
+        "data-slug-hash": "wLqEPe",
+        "data-default-tab": "result",
+        "data-user": "TFcuse",
+        "data-embed-version": 2,
+        "data-pen-title": "Bridge-ECL.2",
+        placeholder: `
+          See the Pen <a href="https://codepen.io/TFcuse/pen/wLqEPe">here</a>.
+          `,
+        wrapperClass: "codepen-embed"
+      }
+    };
+  }
+};
 </script>
